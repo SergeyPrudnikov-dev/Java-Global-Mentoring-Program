698ea4acc363a917edbb9bf0ed85bf9ed447e7e5
Prudnikau Siarhei <sprudnikau@copyright.com>
2022-07-23 18:29:59 +0200

    Added guava-cache service2

--- guava-cache-service/src/main/java/com/prudnikau/guava/cache/CacheThread.java
+++ guava-cache-service/src/main/java/com/prudnikau/guava/cache/CacheThread.java
@@ -1,31 +1,34 @@
+package com.prudnikau.guava.cache;
+
+import com.prudnikau.guava.data.Data;
+
 import java.util.UUID;
 import java.util.logging.Logger;
 
 /**
- * //TODO: [before commit] class description.
  * <p/>
- * University-commission 2022  epam.com
+ * Guava-cache-service  2022  epam.com
  * <p/>
  * Date: 07/14/2022
  *
  * @author Siarhei Prudnikau1
  */
 public class CacheThread extends Thread {
-    private static final Logger LOGGER = Logger.getLogger(Cache.class.getName());
-    private final Cache cache;
+    private static final Logger LOGGER = Logger.getLogger(GuavaCache.class.getName());
+    private final GuavaCache cache;
     private final int prefix;
 
 
-    public CacheThread(Cache cache, int prefix) {
+    public CacheThread(GuavaCache cache, int prefix) {
         this.cache = cache;
         this.prefix = prefix;
     }
 
     public void run() {
         for (int i = 0; i < 15000; i++) {
-            Data data = new Data(prefix + "_" + i + "_" + UUID.randomUUID());
-            cache.put(data);
+            cache.getUnchecked(prefix + "_" + i + "_" + UUID.randomUUID());
         }
-        LOGGER.info(prefix + " thread " + cache.getStatistic());
+        LOGGER.info(prefix + " thread ");
+        cache.cleanUp();
     }
 }
--- guava-cache-service/src/test/java/com/prudnikau/guava/cache/GuavaCacheUnitTest.java
+++ guava-cache-service/src/test/java/com/prudnikau/guava/cache/GuavaCacheUnitTest.java
@@ -1,27 +1,14 @@
 package com.prudnikau.guava.cache;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
+import com.prudnikau.guava.data.Data;
 import org.junit.Test;
 
-import com.google.common.base.Optional;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.cache.RemovalCause;
-import com.google.common.cache.RemovalListener;
-import com.google.common.cache.RemovalNotification;
-import com.google.common.cache.Weigher;
 /**
- * //TODO: [before commit] class description.
  * <p/>
- * University-commission 2022  epam.com
+ * Guava-cache-service  2022  epam.com
  * <p/>
  * Date: 07/19/2022
  *
@@ -30,191 +17,36 @@ import com.google.common.cache.Weigher;
 public class GuavaCacheUnitTest {
 
     @Test
-    public void whenCacheMiss_thenAutoLoad() {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().build(loader);
-        assertEquals(0, cache.size());
-        assertEquals("HELLO", cache.getUnchecked("hello"));
-        assertEquals(1, cache.size());
-    }
-
-    @Test
-    public void whenCacheReachMaxSize_thenEviction() {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().maximumSize(3).build(loader);
+    public void whenEntryIsUsedOnceEvery4SecondsThenItRemainsInCache() throws InterruptedException {
+        long timeSinceLastUse = 5;
+        int maxSize = 100000;
+        Data lastData = new Data("last");
+        final GuavaCache cache = new GuavaCache(timeSinceLastUse, maxSize);
         cache.getUnchecked("first");
-        cache.getUnchecked("second");
-        cache.getUnchecked("third");
-        cache.getUnchecked("forth");
-        assertEquals(3, cache.size());
-        assertNull(cache.getIfPresent("first"));
-        assertEquals("FORTH", cache.getIfPresent("forth"));
-    }
-
-    @Test
-    public void whenCacheReachMaxWeight_thenEviction() {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final Weigher<String, String> weighByLength = new Weigher<String, String>() {
-            @Override
-            public int weigh(final String key, final String value) {
-                return value.length();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().maximumWeight(16).weigher(weighByLength).build(loader);
-        cache.getUnchecked("first");
-        cache.getUnchecked("second");
-        cache.getUnchecked("third");
         cache.getUnchecked("last");
-        assertEquals(3, cache.size());
-        assertNull(cache.getIfPresent("first"));
-        assertEquals("LAST", cache.getIfPresent("last"));
-    }
 
-    @Test
-    public void whenEntryIdle_thenEviction() throws InterruptedException {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().expireAfterAccess(2, TimeUnit.MILLISECONDS).build(loader);
-        cache.getUnchecked("hello");
-        assertEquals(1, cache.size());
-        cache.getUnchecked("hello");
-        Thread.sleep(3);
-        cache.getUnchecked("test");
-        assertEquals(1, cache.size());
-        assertNull(cache.getIfPresent("hello"));
-    }
-
-    @Test
-    public void whenEntryLiveTimeExpire_thenEviction() throws InterruptedException {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().expireAfterWrite(2, TimeUnit.MILLISECONDS).build(loader);
-        cache.getUnchecked("hello");
-        assertEquals(1, cache.size());
-        Thread.sleep(3);
-        cache.getUnchecked("test");
-        assertEquals(1, cache.size());
-        assertNull(cache.getIfPresent("hello"));
-    }
-
-    @Test
-    public void whenWeekKeyHasNoRef_thenRemoveFromCache() {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().weakKeys().build(loader);
-    }
-
-    @Test
-    public void whenSoftValue_thenRemoveFromCache() {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().softValues().build(loader);
-    }
-
-    @Test
-    public void whenNullValue_thenOptional() {
-        final CacheLoader<String, Optional<String>> loader = new CacheLoader<String, Optional<String>>() {
-            @Override
-            public final Optional<String> load(final String key) {
-                return Optional.fromNullable(getSuffix(key));
-            }
-        };
-        final LoadingCache<String, Optional<String>> cache = CacheBuilder.newBuilder().build(loader);
-        assertEquals("txt", cache.getUnchecked("text.txt").get());
-        assertFalse(cache.getUnchecked("hello").isPresent());
-    }
-
-    @Test
-    public void whenLiveTimeEnd_thenRefresh() {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().refreshAfterWrite(1, TimeUnit.MINUTES).build(loader);
-    }
-
-    @Test
-    public void whenPreloadCache_thenUsePutAll() {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().build(loader);
-        final Map<String, String> map = new HashMap<String, String>();
-        map.put("first", "FIRST");
-        map.put("second", "SECOND");
-        cache.putAll(map);
-        assertEquals(2, cache.size());
-    }
-
-    @Test
-    public void whenEntryRemovedFromCache_thenNotify() {
-        final CacheLoader<String, String> loader = new CacheLoader<String, String>() {
-            @Override
-            public final String load(final String key) {
-                return key.toUpperCase();
-            }
-        };
-        final RemovalListener<String, String> listener = new RemovalListener<String, String>() {
-            @Override
-            public void onRemoval(final RemovalNotification<String, String> n) {
-                if (n.wasEvicted()) {
-                    final String cause = n.getCause().name();
-                    assertEquals(RemovalCause.SIZE.toString(), cause);
-                }
-            }
-        };
-        final LoadingCache<String, String> cache = CacheBuilder.newBuilder().maximumSize(3).removalListener(listener).build(loader);
-        cache.getUnchecked("first");
-        cache.getUnchecked("second");
-        cache.getUnchecked("third");
-        cache.getUnchecked("last");
-        assertEquals(3, cache.size());
-    }
-
-    // UTIL
-
-    private String getSuffix(final String str) {
-        final int lastIndex = str.lastIndexOf('.');
-        if (lastIndex == -1) {
-            return null;
+        for (int i =0; i < 3; i++) {
+            cache.getUnchecked("last");
+            Thread.sleep(4000);
+            cache.cleanUp();
         }
-        return str.substring(lastIndex + 1);
+        long size = cache.size();
+        assertEquals(1, size);
+        assertNull(cache.getIfPresent("first"));
+        assertEquals(lastData, cache.getIfPresent("last"));
     }
 
+    @Test
+    public void whenRecordsNotUsedForMore5Seconds_thenRemovedAll() throws InterruptedException {
+        long timeSinceLastUse = 5;
+        int maxSize = 100000;
+        final GuavaCache cache = new GuavaCache(timeSinceLastUse, maxSize);
+        int threads = 10;
+        for (int i=0; i<threads; i++) {
+            new CacheThread(cache, i).start();
+        }
+        Thread.sleep(10000);
+        cache.cleanUp();
+        assertEquals(0, (long)cache.size());
+    }
 }
